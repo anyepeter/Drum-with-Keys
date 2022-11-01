17e20ba7effab6203ad787d3a19b66ea3505cfca
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-11-01 22:12:29 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('keydown', (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+});
