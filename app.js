a9c8ec1fed740339a0241e675ea1e320f3cadb3d
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-11-02 20:14:04 +0100

    update the app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 document.addEventListener('keydown', (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const keys = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (!audio) return;
-
   audio.currentTime = 0;
+  keys.classList.add('play-color');
   audio.play();
+  setTimeout(() => {
+    keys.classList.remove('play-color');
+  }, 50);
 });
