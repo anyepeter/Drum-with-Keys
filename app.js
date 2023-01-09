2c7ffa2782b248069e87afd3b0f6bf725fda8783
anyepeter <petzyrockchendi@gmail.com>
2023-01-09 23:13:57 +0100

    fix linter

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Using keyboard
 window.addEventListener('keydown', (e) => {
   const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
   const buttonPress = document.querySelector(`div[data-key="${e.keyCode}"]`);
@@ -11,13 +12,19 @@ window.addEventListener('keydown', (e) => {
   }, 50);
 });
 
+//using click event handler
 const buttonEl = document.querySelectorAll('div');
 buttonEl.forEach((element) =>{
   element.addEventListener('click', ()=>{
     const audio = document.querySelectorAll(`audio`);
     audio.forEach((items) => {
       if(items.dataset.key == element.dataset.key){
+        items.currentTime = 0;
         items.play();
+        element.classList.add('play-color');
+        setTimeout(() => {
+          element.classList.remove('play-color');
+        }, 50);
       }
     })
   })
